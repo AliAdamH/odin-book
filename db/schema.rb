aa6df14b8929d5d4020d62a9afd7554d9b569968
Ali Hassaine <ali.hs@outlook.fr>
2022-04-10 08:15:47 +0000

    Add Friendship model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_10_080805) do
+ActiveRecord::Schema.define(version: 2022_04_10_081413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2022_04_10_080805) do
   create_table "friendship_requests", force: :cascade do |t|
     t.integer "requester_id"
     t.integer "addressee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
