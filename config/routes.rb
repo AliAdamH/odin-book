d7a4c2f23f22f5d1b0b9507b1ef3878b769717f5
Ali Hassaine <ali.hs@outlook.fr>
2022-04-10 15:38:16 +0000

    Add friendship routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
   resources :users, only: %i[index show]
+  resources :friendship_requests, only: %i[create destroy]
+  resources :friendships, only: %i[create]
 end
