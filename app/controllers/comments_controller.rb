05e8bc85c63803fea69cce18d01455fd06c8511e
Ali Hassaine <ali.hs@outlook.fr>
2022-04-11 22:47:47 +0000

    Setup comment controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+
+  def create
+    @comment = curren_user.comments.build(comment_params)
+    unless @comment.save
+      flash[:alert] = 'Failed to register comment'
+    end
+
+    redirect_back fallback_location: post_path(params[:post_id])
+  end
+
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:content).merge(post_id: params[:post_id])
+  end
+end
