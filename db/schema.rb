082621f3b8e478ffaed84265e0f4c6510b8bb8c3
Ali Hassaine <ali.hs@outlook.fr>
2022-04-13 10:38:51 +0000

    Add comments counter cache

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_13_102359) do
+ActiveRecord::Schema.define(version: 2022_04_13_103518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2022_04_13_102359) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.integer "likes_count"
+    t.integer "comments_count"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
