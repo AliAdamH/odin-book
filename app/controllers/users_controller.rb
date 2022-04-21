64277dc902790682c8d7d9f37ecac09809e74225
Ali Hassaine <ali.hs@outlook.fr>
2022-04-21 16:14:55 +0000

    Add user profile view with edit feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,27 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @user_posts = @user.posts
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+    respond_to do |format|
+      if current_user.update(user_params)
+        format.html { redirect_to user_url(current_user), notice: "You have updated your profile." }
+        format.json { render :show, status: :ok, location: current_user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: current_user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :picture)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
-  resources :users, only: %i[index show]
+  resources :users, only: %i[index show edit update]
   resources :friendship_requests, only: %i[create destroy]
   resources :friendships, only: %i[create]
 
